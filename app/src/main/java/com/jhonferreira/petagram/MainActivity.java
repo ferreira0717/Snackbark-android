5552e9e371665d738b072aeccd1ffd0283ce26ff
Jhon Ferreira <ferreira0717@gmail.com>
2022-09-01 15:53:34 -0500

    primer commit

---
+++ app/src/main/java/com/jhonferreira/petagram/MainActivity.java
@@ -0,0 +1,42 @@
+package com.jhonferreira.petagram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        agregarFAB();
+    }
+
+    public void agregarFAB() {
+        FloatingActionButton miFAB = (FloatingActionButton) findViewById(R.id.fabMiFAB);
+        miFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(getBaseContext(), getResources().getString(R.string.mensaje), Toast.LENGTH_SHORT).show();
+                Snackbar.make(v,getResources().getString(R.string.mensaje),Snackbar.LENGTH_SHORT)
+                        .setAction(getResources().getString(R.string.texto_accion), new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Log.i("SNACKBAR", "CLICK EN SNACKBAR");
+                            }
+                        })
+                        .setActionTextColor(getResources().getColor(R.color.Naranja))
+                        .show();
+            }
+        });
+    }
+
+}
